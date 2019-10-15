9d1ec8f6486b6664dd80f0000bb1d00b99c4c10e
Ronny <ronny.rook@labela.nl>
2019-10-15 13:45:47 +0200

    Fix Android duplicate res folder and and remove fabric.io

--- android/app/src/main/java/com/reactprimenative/MainApplication.java
+++ android/app/src/main/java/com/reactprimenative/MainApplication.java
@@ -2,6 +2,7 @@ package com.reactprimenative;
 
 import android.app.Application;
 import android.content.Context;
+import com.facebook.react.BuildConfig;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
