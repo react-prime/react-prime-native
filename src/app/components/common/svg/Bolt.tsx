47e545f29dbd5083195d1fed3a99c5b2e8caa75f
Ronny <ronny.rook@labela.nl>
2020-01-17 14:06:47 +0100

    Change SVG props and SvgWrapper

--- src/app/components/common/svg/Bolt.tsx
+++ src/app/components/common/svg/Bolt.tsx
@@ -3,16 +3,14 @@ import { Path } from 'react-native-svg';
 
 import SvgWrapper from './SvgWrapper';
 
-const Bolt: React.FC<BoltProps> = ({ fill, height, width }) => (
-  <SvgWrapper {...{ height, width }}>
-    <Path d="M27.5 12.9l-25 24.3L10 19.4H0L7.5 0h20L15 12.9z" fill={fill} />
+const Bolt: React.FC<BoltProps> = (props) => (
+  <SvgWrapper {...props} viewBox="0 0 28 35">
+    <Path d="M27.5 12.9l-25 24.3L10 19.4H0L7.5 0h20L15 12.9z" fill={props.fill} />
   </SvgWrapper>
 );
 
 type BoltProps = {
   fill: string;
-  height: number;
-  width: number;
 }
 
 export default Bolt;
--- src/app/components/common/svg/SvgWrapper.tsx
+++ src/app/components/common/svg/SvgWrapper.tsx
@@ -7,25 +7,29 @@ const IconWrapper = styled.View`
   justify-content: center;
 `;
 
-const SvgWrapper: React.FC<SvgWrapperProps> = ({ width, height, children }) => {
-  const viewBoxRatio = width / height;
-
+const SvgWrapper: React.FC<SvgWrapperProps> = ({
+  width, height, fill, stroke, viewBox, ...props
+}) => {
   return (
     <IconWrapper>
       <Svg
-        width={width || (height && height * viewBoxRatio)}
-        height={height || (width && width / viewBoxRatio)}
-        viewBox={`0 0 ${width} ${height}`}
+        width={width}
+        height={height}
+        viewBox={viewBox}
       >
-        {children}
+        {React.cloneElement(props.children, { fill, stroke })}
       </Svg>
     </IconWrapper>
   );
 };
 
 type SvgWrapperProps = {
-  height: number;
-  width: number;
+  children: any;
+  fill?: string;
+  stroke?: string;
+  height?: number;
+  width?: number;
+  viewBox: string;
 }
 
 export default SvgWrapper;
