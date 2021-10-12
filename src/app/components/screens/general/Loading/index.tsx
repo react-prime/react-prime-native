f6677729ae2d8463129957f5bfe7cbdf68e7c0c3
Ronny Rook <ronny@labela.nl>
2021-10-12 22:55:07 +0200

    Fix loading useNavigation typing

--- src/app/components/screens/general/Loading/index.tsx
+++ src/app/components/screens/general/Loading/index.tsx
@@ -1,17 +1,20 @@
-import * as i from 'types';
 import React from 'react';
 import { Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 import { LoadingContainer } from './styled';
 
+type LoadingNavigationProps = {
+  Loading: undefined;
+  Main: undefined;
+};
+
 export const Loading: React.FC = () => {
-  const navigation = useNavigation<i.DashboardNavigation>();
+  const navigation = useNavigation<StackNavigationProp<LoadingNavigationProps, 'Loading'>>();
 
   React.useEffect(() => {
-    setTimeout(() => {
-      navigation.navigate('DashboardOverview');
-    }, 2000);
+    navigation.navigate('Main');
   }, []);
 
   return (
