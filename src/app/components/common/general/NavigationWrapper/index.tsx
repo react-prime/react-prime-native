76078adf576991ce09bbd6de43211344c11e16e8
Ronny Rook <ronny@labela.nl>
2021-12-24 11:08:54 +0100

    Update packages with Babel support

--- src/app/components/common/general/NavigationWrapper/index.tsx
+++ src/app/components/common/general/NavigationWrapper/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { useFlipper } from '@react-navigation/devtools';
 
 import theme from 'styles/theme';
 
@@ -8,7 +7,6 @@ export const NavigationWrapper: React.FC = ({
   children,
 }) => {
   const navigationRef = React.useRef(null);
-  useFlipper(navigationRef);
 
   const linking = {
     prefixes: ['reactprimenative://'],
